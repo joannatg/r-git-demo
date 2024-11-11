22040b77e905c6baf310d1429f79141cfa19994c
Joanna Georgescu <jptgeorgescu@gmail.com>
2024-11-11 08:08:21 -0800

    adjusted code to pull pats AND encounters

--- getPatients.R
+++ getPatients.R
@@ -18,16 +18,18 @@ library(devtools)
 
 ##------------------------------------------------------------------------------------------------------------------------------------------
 
-#notes with encsIDs
+#pats and encs
 dbHandleA <- odbcDriverConnect("driver={SQL Server}; server=EsyRsrchSQL01; database=research_dev; uid=georgescuj; pwd=NewPasswordHere")
-pats <- sqlQuery(dbHandleA, "SELECT  DISTINCT d.patid, d.BIRTH_DATE, d.SEX, e.ADMIT_DATE, e.encounterid, cast(floor(datediff(day,cast(d.BIRTH_DATE  as date),cast('01-01-2023' as date))/365.25) as varchar) AS ageOn01Jan2023
-                                      --CONCAT(cast(SYSTOLIC as INT), '/', cast(DIASTOLIC AS INT)) AS BLOOD_PRESSURE
-                               FROM ochin.dbo.DEMOGRAPHIC d 
-                                      INNER JOIN #encs e ON d.patid=e.patid
-                                      LEFT JOIN ochin.dbo.VITAL v ON e.patid=v.patid AND e.ENCOUNTERID=v.ENCOUNTERID
-                              WHERE  d.BIRTH_DATE < '01-01-2020' 
-                                      AND v.SYSTOLIC is not NULL
-                                      AND v.DIASTOLIC is not NULL
-                   AND ADMIT_DATE BETWEEN '01-01-2013' AND '12-31-2023' ")
+pats <- sqlQuery(dbHandleA, "SELECT DISTINCT d.patid, d.BIRTH_DATE, d.SEX, e.ADMIT_DATE, e.encounterid, 
+                             cast(floor(datediff(day,cast(d.BIRTH_DATE  as date),cast('01-01-2023' as date))/365.25) as varchar) AS ageOn01Jan2023
+                              FROM ochin.dbo.DEMOGRAPHIC d 
+                                LEFT JOIN ochin.dbo.ENCOUNTER e ON d.patid=e.patid
+                                LEFT JOIN ochin.dbo.VITAL v ON e.patid=v.patid AND e.ENCOUNTERID=v.ENCOUNTERID
+                                WHERE  e.ADMIT_DATE BETWEEN '01-01-2013' AND '12-31-2023'
+                                AND d.BIRTH_DATE < '01-01-2020' 
+                                AND v.SYSTOLIC is not NULL
+                                AND v.DIASTOLIC is not NULL ")
 close(dbHandleA)
-patsDF <- data.frame(npats)
+patsDF <- data.frame(pats)
+
+
